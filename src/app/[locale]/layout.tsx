166df7ec217fe26d69ad7cdb6e4569f6d0159d54
Mr Sharafdin <isasharafdin@gmail.com>
2025-03-17 07:36:31 +0300

    Add Google Analytics tracking code (GA4) to site

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,69 +1,75 @@
 import "@/once-ui/styles/index.scss";
 import "@/once-ui/tokens/index.scss";
 
-import classNames from 'classnames';
+import classNames from "classnames";
 
 import { Footer, Header, RouteGuard } from "@/components";
-import { baseURL, effects, style } from '@/app/resources'
+import { baseURL, effects, style } from "@/app/resources";
 
-import { Inter, Jost } from 'next/font/google'
-import { Sora } from 'next/font/google';
-import { Source_Code_Pro } from 'next/font/google';
+import { Inter, Jost } from "next/font/google";
+import { Sora } from "next/font/google";
+import { Source_Code_Pro } from "next/font/google";
+import Script from "next/script";
 
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages, getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+import { NextIntlClientProvider } from "next-intl";
+import {
+  getMessages,
+  getTranslations,
+  unstable_setRequestLocale,
+} from "next-intl/server";
 
 import { routing } from "@/i18n/routing";
 import { renderContent } from "@/app/resources";
 import { Background, Flex } from "@/once-ui/components";
 
-export async function generateMetadata(
-	{ params: { locale }}: { params: { locale: string }}
-) {
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = await getTranslations();
+  const { person, home } = renderContent(t);
 
-	const t = await getTranslations();
-	const { person, home } = renderContent(t);
-
-	return {
-		metadataBase: new URL(`https://${baseURL}/${locale}`),
-		title: home.title,
-		description: home.description,
-		openGraph: {
-			title: `${person.firstName}'s Portfolio`,
-			description: 'Portfolio website showcasing my work.',
-			url: baseURL,
-			siteName: `${person.firstName}`,
-			locale: 'en_US',
-			type: 'website',
-			images: [
-				{
-				  url: 'https://avatars.githubusercontent.com/u/83120892?v=4',
-				  alt: "ISMAILAINTE's github image",
-				},
-			],
-		},
-		robots: {
-			index: true,
-			follow: true,
-			googleBot: {
-				index: true,
-				follow: true,
-				'max-video-preview': -1,
-				'max-image-preview': 'large',
-				'max-snippet': -1,
-			},
-		},
-	}
-};
+  return {
+    metadataBase: new URL(`https://${baseURL}/${locale}`),
+    title: home.title,
+    description: home.description,
+    openGraph: {
+      title: `${person.firstName}'s Portfolio`,
+      description: "Portfolio website showcasing my work.",
+      url: baseURL,
+      siteName: `${person.firstName}`,
+      locale: "en_US",
+      type: "website",
+      images: [
+        {
+          url: "https://avatars.githubusercontent.com/u/83120892?v=4",
+          alt: "ISMAILAINTE's github image",
+        },
+      ],
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+  };
+}
 
 const primary = Inter({
-	variable: '--font-primary',
-	subsets: ['latin'],
-	display: 'swap',
-})
+  variable: "--font-primary",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 type FontConfig = {
-    variable: string;
+  variable: string;
 };
 
 /*
@@ -71,88 +77,109 @@ type FontConfig = {
 	from https://once-ui.com/customize
 */
 
-
 const secondary = Jost({
-    variable: '--font-secondary',
-    subsets: ['latin'],
-    display: 'swap'
+  variable: "--font-secondary",
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const tertiary = Sora({
-    variable: '--font-tertiary',
-    subsets: ['latin'],
-    display: 'swap'
+  variable: "--font-tertiary",
+  subsets: ["latin"],
+  display: "swap",
 });
 
 /*
-*/
+ */
 
 const code = Source_Code_Pro({
-	variable: '--font-code',
-	subsets: ['latin'],
-	display: 'swap',
+  variable: "--font-code",
+  subsets: ["latin"],
+  display: "swap",
 });
 
 interface RootLayoutProps {
-	children: React.ReactNode;
-	params: {locale: string};
+  children: React.ReactNode;
+  params: { locale: string };
 }
 
 export function generateStaticParams() {
-	return routing.locales.map((locale) => ({locale}));
-  }
+  return routing.locales.map((locale) => ({ locale }));
+}
 
 export default async function RootLayout({
-	children,
-	params: {locale}
-} : RootLayoutProps) {
-	unstable_setRequestLocale(locale);
-	const messages = await getMessages();
-	return (
-		<NextIntlClientProvider messages={messages}>
-			<Flex
-				as="html" lang="en"
-				background="page"
-				data-neutral={style.neutral} data-brand={style.brand} data-accent={style.accent}
-				data-solid={style.solid} data-solid-style={style.solidStyle}
-				data-theme={style.theme}
-				data-border={style.border}
-				data-surface={style.surface}
-				data-transition={style.transition}
-				className={classNames(
-					primary.variable,
-					secondary ? secondary.variable : '',
-					tertiary ? tertiary.variable : '',
-					code.variable)}>
-				<Flex style={{minHeight: '100vh'}}
-					as="body"
-					fillWidth margin="0" padding="0"
-					direction="column">
-					<Background
-						mask={effects.mask as any}
-						gradient={effects.gradient as any}
-						dots={effects.dots as any}
-						lines={effects.lines as any}/>
-					<Flex
-						fillWidth
-						minHeight="16">
-					</Flex>
-					<Header/>
-					<Flex
-						zIndex={0}
-						fillWidth paddingY="l" paddingX="l"
-						justifyContent="center" flex={1}>
-						<Flex
-							justifyContent="center"
-							fillWidth minHeight="0">
-							<RouteGuard>
-								{children}
-							</RouteGuard>
-						</Flex>
-					</Flex>
-					<Footer/>
-				</Flex>
-			</Flex>
-		</NextIntlClientProvider>
-	);
+  children,
+  params: { locale },
+}: RootLayoutProps) {
+  unstable_setRequestLocale(locale);
+  const messages = await getMessages();
+  return (
+    <NextIntlClientProvider messages={messages}>
+      <Flex
+        as="html"
+        lang="en"
+        background="page"
+        data-neutral={style.neutral}
+        data-brand={style.brand}
+        data-accent={style.accent}
+        data-solid={style.solid}
+        data-solid-style={style.solidStyle}
+        data-theme={style.theme}
+        data-border={style.border}
+        data-surface={style.surface}
+        data-transition={style.transition}
+        className={classNames(
+          primary.variable,
+          secondary ? secondary.variable : "",
+          tertiary ? tertiary.variable : "",
+          code.variable
+        )}
+      >
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-V5VRNMTRLN"
+        />
+        <Script id="google-analytics">
+          {`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						
+						gtag('config', 'G-V5VRNMTRLN');
+					`}
+        </Script>
+        <Flex
+          style={{ minHeight: "100vh" }}
+          as="body"
+          fillWidth
+          margin="0"
+          padding="0"
+          direction="column"
+        >
+          <Background
+            mask={effects.mask as any}
+            gradient={effects.gradient as any}
+            dots={effects.dots as any}
+            lines={effects.lines as any}
+          />
+          <Flex fillWidth minHeight="16"></Flex>
+          <Header />
+          <Flex
+            zIndex={0}
+            fillWidth
+            paddingY="l"
+            paddingX="l"
+            justifyContent="center"
+            flex={1}
+          >
+            <Flex justifyContent="center" fillWidth minHeight="0">
+              <RouteGuard>{children}</RouteGuard>
+            </Flex>
+          </Flex>
+          <Footer />
+        </Flex>
+      </Flex>
+    </NextIntlClientProvider>
+  );
 }
